ad8fe8a21ccb3165ce75ae810bc381f98542d8d6
llNovall <25434267+llNovall@users.noreply.github.com>
2021-05-29 17:22:40 +0200

    Updated PlayerMovementController which can now move in all direction and face mouse

--- Assets/Scripts/Input/PlayerMovementController.cs
+++ Assets/Scripts/Input/PlayerMovementController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
@@ -9,108 +10,72 @@ public class PlayerMovementController : MonoBehaviour
     private PlayerInputController _playerInputController;
 
     [SerializeField]
-    private bool _isAbleToMove, _isGrounded;
+    private bool _isAbleToMove;
 
     [SerializeField]
     private float _movementSpeed;
 
     [SerializeField]
-    private float _jumpSpeed, _jumpScaler;
-
-    [SerializeField]
-    private float _defaultGravity;
-
-    [SerializeField]
-    private Vector3 _rayCastOffset;
+    private float _minDistanceFromWall;
 
     [SerializeField]
     private Rigidbody2D _rigidbody2D;
+
+    private Vector2 _movement;
+
+    [SerializeField]
+    private float _offset;
 
     private void Awake()
     {
         if(!(_rigidbody2D = gameObject.GetComponent<Rigidbody2D>()))
             _rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
 
-        _rigidbody2D.mass = 2;
-        _rigidbody2D.drag = 1;
+        _rigidbody2D.isKinematic = true;
     }
     private void Start()
     {
         _playerInputController = gameObject.GetComponent<PlayerInputController>();
         _playerInputController.SubscribeToOnPlayerMovementInputUpdated(PlayerInputController_OnPlayerMovementInputUpdated);
-        _playerInputController.SubscribeToOnJump(PlayerInputController_OnJump);
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-        RaycastHit2D[] hits = Physics2D.RaycastAll(gameObject.transform.position + _rayCastOffset, -gameObject.transform.up, 1);
-
-        if (hits.Length > 0)
-        {
-            for (int i = 0; i < hits.Length; i++)
-            {
-                if (hits[i].collider.gameObject != gameObject)
-                {
-                    _isGrounded = true;
-                    break;
-                }
-            }
-        }
-        else
-            _isGrounded = false;
-        
+        FaceMouse();
     }
-    private void PlayerInputController_OnJump()
-    {
-        if (_isGrounded)
-        {
-            if (_isAbleToMove)
-            {
-                _rigidbody2D.AddForce(Vector2.up * _jumpSpeed * _jumpScaler);
-            }
-        }
-    }
-
-    private void PlayerInputController_OnPlayerMovementInputUpdated(float movementInput)
+    private void PlayerInputController_OnPlayerMovementInputUpdated(Vector2 movementInput)
     {
         if (_isAbleToMove)
         {
-            float sideMovement = movementInput * _movementSpeed;
-            //gameObject.transform.position += new Vector3(sideMovement, 0, 0);
+            Vector2 movement = movementInput * _movementSpeed * Time.deltaTime;
+            _movement = movement;
 
-            _rigidbody2D.AddForce(new Vector2(sideMovement, 0));
- 
+            int layerMask = LayerMask.GetMask("Walls");
+            bool isHit = Physics2D.Raycast(gameObject.transform.position, movementInput, _minDistanceFromWall, layerMask);
+
+            if (isHit)
+                return;
+            gameObject.transform.position += new Vector3(movement.x, movement.y,0);
         }
     }
-
-    public void EnableGravity(bool isEnabled)
+    private void FaceMouse()
     {
-        if (isEnabled)
-            _rigidbody2D.gravityScale = _defaultGravity;
-        else
-            _rigidbody2D.gravityScale = 0;
-
-        //if (isEnabled)
-        //    _rigidbody2D.isKinematic = false;
-        //else
-        //    _rigidbody2D.isKinematic = true;
-    }
-    public void EnableMovement(bool isEnabled)
-    {
-        _isAbleToMove = isEnabled;
-
-        if (!_isAbleToMove)
+        if (_isAbleToMove)
         {
-            _rigidbody2D.velocity = Vector2.zero;
-            _rigidbody2D.angularVelocity = 0;
-        }
-            
+
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - gameObject.transform.position;
+            direction = direction.normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            gameObject.transform.rotation = Quaternion.Euler(0,0,angle + _offset);
+        }     
     }
+
+    public void EnableMovement(bool isEnabled) => _isAbleToMove = isEnabled;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Ray ray = new Ray(gameObject.transform.position + _rayCastOffset, Vector3.down);
+        Ray ray = new Ray(gameObject.transform.position, _movement);
         Gizmos.DrawRay(ray);
     }
 }
