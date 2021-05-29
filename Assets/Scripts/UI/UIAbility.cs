54f314f2c0ae4458a0a97df8a115f00e94bccd00
llNovall <25434267+llNovall@users.noreply.github.com>
2021-05-29 15:53:31 +0200

    Added UI script for Ability

---
+++ Assets/Scripts/UI/UIAbility.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAbility : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _txtCooldown, _txtKey;
+
+    [SerializeField]
+    private Image _imgCooldown;
+
+    [SerializeField]
+    private Ability _ability;
+    public void Initialize(Ability ability)
+    {
+        _ability = ability;
+
+        AbilityData abilityData = ability.GetAbilityData();
+
+        _txtCooldown.text = "0";
+        _imgCooldown.gameObject.SetActive(false);
+        ability.SubscribeToOnCooldownUpdated(Ability_OnCooldownUpdated);
+
+        _txtKey.text = abilityData.ActivationKey;
+    }
+
+    private void Ability_OnCooldownUpdated(float cooldown)
+    {
+        _imgCooldown.gameObject.SetActive(cooldown > 0);
+
+        AbilityData abilityData = _ability.GetAbilityData();
+
+        _imgCooldown.fillAmount = abilityData.Cooldown / abilityData.CooldownRequired;
+
+        _txtCooldown.text = Math.Round(cooldown, 1).ToString();
+
+    }
+}
