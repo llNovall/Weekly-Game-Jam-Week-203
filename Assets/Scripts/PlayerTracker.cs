a9e24c5310643abf18bece4eb9225cebadd44659
llNovall <25434267+llNovall@users.noreply.github.com>
2021-05-29 15:53:06 +0200

    Added PlayerTracker script which keeps track of player and its important scripts

---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    public static PlayerTracker Current;
+    public static Health PlayerHealth;
+    public static PlayerMovementController PlayerMovementController;
+    public static PlayerInputController PlayerInputController;
+    public static PlayerAbilitiesController PlayerAbilitiesController;
+    private void Awake()
+    {
+        Current = this;
+        PlayerHealth = gameObject.GetComponent<Health>();
+        PlayerMovementController = gameObject.GetComponent<PlayerMovementController>();
+        PlayerInputController = gameObject.GetComponent<PlayerInputController>();
+        PlayerAbilitiesController = gameObject.GetComponent<PlayerAbilitiesController>();
+    }
+}
